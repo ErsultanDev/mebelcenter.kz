61c5a0e06b87effe705d41c1834d25f9aab18539
Ersultan <example@gmail.com>
2022-12-27 17:41:40 +0300

    ee

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@ if (bg_bot_1 != null) {
             <img class="content_card_img_two" src="images/menu66.png" alt="">
           </div>
           <div class="content_card_text">
-            <h4>Ресепш, барные стойки</h4>
+            <h4>Ресепшн, барные стойки</h4>
           </div>
         </a>
       </div>
@@ -139,7 +139,7 @@ function displayList(e) {
         </div>
       </a>
     </div>
-    <a href="kuhni.html">
+    <a href=${e.link}>
       <div class="product-description">
         <div class="product-label">
           <div class="product-name">
@@ -168,10 +168,9 @@ function displayList(e) {
           </div>
         </div>
       </div></a>
-
       `),
       productRow.appendChild(i);
-      listViewDiv.appendChild(bg_bot_1)
+    listViewDiv.appendChild(bg_bot_1);
   });
 }
 const body = document.querySelector('body'),
@@ -197,6 +196,7 @@ const body = document.querySelector('body'),
   b4_item = document.querySelectorAll('.b4_item'),
   pictures_box_slider = document.querySelector('.Pictures-box_slider'),
   our_page_Slider = document.querySelector('.our_page_Slider'),
+  adBlock_b3_slider = document.querySelector('.adBlock_b3_slider'),
   action_slider = document.querySelector('.action_slider');
 
 contact_slider &&
@@ -361,7 +361,15 @@ if (our_page_Slider != null) {
     ],
   });
 }
-
+if(adBlock_b3_slider != null){
+  $('.adBlock_b3_slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    infinite: true,
+  });
+}
 // Connect
 
 const linkAwesome = document.createElement('link');
@@ -2151,7 +2159,7 @@ productData29 = [
   },
   {
     link: 'prixozhie.html',
-    title: 'Кухни',
+    title: 'Прихожие',
     price: 85320,
     img: 'images/img5.jpg',
   },
@@ -2184,11 +2192,7 @@ productData29 = [
 ];
 
 productData30 = [
-  {
-    link: '',
-    title: '',
-    img: '',
-  },
+
 ];
 productData31 = [
   {
@@ -2752,3 +2756,254 @@ accordions.forEach((accordion) => {
     panel.style.setProperty('--height', `${panel.scrollHeight}px`);
   });
 });
+
+// window.onload = function () {
+//   Particles.init({
+//     selector: ".background"
+//   });
+// };
+// const particles = Particles.init({
+//   selector: ".background",
+//   color: ["#03dac6", "#ff0266", "#000000"],
+//   connectParticles: true,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       options: {
+//         color: ["#faebd7", "#03dac6", "#ff0266"],
+//         maxParticles: 43,
+//         connectParticles: false
+//       }
+//     }
+//   ]
+// });
+
+
+class NavigationPage {
+  constructor() {
+    this.currentId = null;
+    this.currentTab = null;
+    this.tabContainerHeight = 70;
+    this.lastScroll = 0;
+    let self = this;
+    $(".nav-tab").click(function () {
+      self.onTabClick(event, $(this));
+    });
+    $(window).scroll(() => {
+      this.onScroll();
+    });
+    $(window).resize(() => {
+      this.onResize();
+    });
+  }
+
+  onTabClick(event, element) {
+    event.preventDefault();
+    let scrollTop =
+      $(element.attr("href")).offset().top - this.tabContainerHeight + 1;
+    $("html, body").animate({ scrollTop: scrollTop }, 600);
+  }
+
+  onScroll() {
+    this.checkHeaderPosition();
+    this.findCurrentTabSelector();
+    this.lastScroll = $(window).scrollTop();
+  }
+
+  onResize() {
+    if (this.currentId) {
+      this.setSliderCss();
+    }
+  }
+
+  checkHeaderPosition() {
+    const headerHeight = 75;
+    if ($(window).scrollTop() > headerHeight) {
+      $(".nav-container").addClass("nav-container--scrolled");
+    } else {
+      $(".nav-container").removeClass("nav-container--scrolled");
+    }
+    let offset =
+      $(".nav").offset().top +
+      $(".nav").height() -
+      this.tabContainerHeight -
+      headerHeight;
+    if (
+      $(window).scrollTop() > this.lastScroll &&
+      $(window).scrollTop() > offset
+    ) {
+      $(".nav-container").addClass("nav-container--move-up");
+      $(".nav-container").removeClass("nav-container--top-first");
+      $(".nav-container").addClass("nav-container--top-second");
+    } else if (
+      $(window).scrollTop() < this.lastScroll &&
+      $(window).scrollTop() > offset
+    ) {
+      $(".nav-container").removeClass("nav-container--move-up");
+      $(".nav-container").removeClass("nav-container--top-second");
+      $(".nav-container-container").addClass("nav-container--top-first");
+    } else {
+      $(".nav-container").removeClass("nav-container--move-up");
+      $(".nav-container").removeClass("nav-container--top-first");
+      $(".nav-container").removeClass("nav-container--top-second");
+    }
+  }
+
+  findCurrentTabSelector(element) {
+    let newCurrentId;
+    let newCurrentTab;
+    let self = this;
+    $(".nav-tab").each(function () {
+      let id = $(this).attr("href");
+      let offsetTop = $(id).offset().top - self.tabContainerHeight;
+      let offsetBottom =
+        $(id).offset().top + $(id).height() - self.tabContainerHeight;
+      if (
+        $(window).scrollTop() > offsetTop &&
+        $(window).scrollTop() < offsetBottom
+      ) {
+        newCurrentId = id;
+        newCurrentTab = $(this);
+      }
+    });
+    if (this.currentId != newCurrentId || this.currentId === null) {
+      this.currentId = newCurrentId;
+      this.currentTab = newCurrentTab;
+      this.setSliderCss();
+    }
+  }
+
+  setSliderCss() {
+    let width = 0;
+    let left = 0;
+    if (this.currentTab) {
+      width = this.currentTab.css("width");
+      left = this.currentTab.offset().left;
+    }
+    $(".nav-tab-slider").css("width", width);
+    $(".nav-tab-slider").css("left", left);
+  }
+}
+
+new NavigationPage();
+
+particlesJS(
+  'particles-js',
+
+  {
+    particles: {
+      number: {
+        value: 80,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: ["#eea849", "#03dac6", "#ff0266"],
+      },
+      shape: {
+        type: 'circle',
+        stroke: {
+          width: 0,
+          color: '#000000',
+        },
+        polygon: {
+          nb_sides: 5,
+        },
+        image: {
+          src: 'img/github.svg',
+          width: 100,
+          height: 100,
+        },
+      },
+      opacity: {
+        value: 0.5,
+        random: false,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
+      },
+      size: {
+        value: 5,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          size_min: 0.1,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: '#ffffff',
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 5,
+        direction: 'none',
+        random: false,
+        straight: false,
+        out_mode: 'out',
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: 'canvas',
+      events: {
+        onhover: {
+          enable: true,
+          mode: 'grab',
+        },
+        onclick: {
+          enable: true,
+          mode: 'push',
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 120,
+          line_linked: {
+            opacity: 1,
+          },
+        },
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3,
+        },
+        repulse: {
+          distance: 200,
+        },
+        push: {
+          particles_nb: 4,
+        },
+        remove: {
+          particles_nb: 2,
+        },
+      },
+    },
+    retina_detect: true,
+    config_demo: {
+      hide_card: false,
+      background_color: '#ccc',
+      background_image: '',
+      background_position: '50% 50%',
+      background_repeat: 'no-repeat',
+      background_size: 'cover',
+    },
+  },
+);
